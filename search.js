95cda8783e60571bebe4b586e6f72be02d74d521
Eberechi Nwoge <enwoge@microsoft.com>
2023-06-07 15:06:23 +0100

    add serpapi autocomplete reverse proxy serer

---
+++ search.js
@@ -0,0 +1,28 @@
+import { getJson } from "serpapi";
+import dotenv from "dotenv";
+dotenv.config();
+
+const API_KEY = process.env.SERPAPI_KEY;
+
+export async function postResults(req, res, next) {
+  req.params = { ...req.body };
+  next();
+}
+
+export async function getResults(req, res, next) {
+  req.params = req.query;
+  next();
+}
+
+export async function search(req, res, next) {
+  const params = req.params;
+  try {
+    const data = await getJson("google_autocomplete", params);
+    res.locals.result = data;
+    res.status(200);
+  } catch (error) {
+    console.log(error)
+    res.status(400);
+  }
+  next();
+}
---
+++ server.js
@@ -0,0 +1,42 @@
+import express, { json } from "express";
+import dotenv from "dotenv";
+import { config } from "serpapi";
+import cors from "cors";
+import {getResults, postResults, search } from './search.js';
+
+
+
+dotenv.config();
+// console.log(process.env);
+config.api_key = process.env.SERPAPI_KEY;
+
+
+const app = express();
+app.use(cors({ origin: "http://localhost:3001" }));
+app.use(json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "response to default get request" });
+});
+
+app.get("/another-route", (req, res) => {
+  res.status(200).json({ message: "here is another route" });
+});
+
+app.get("/keyword-search/:q", search, (req, res) => {
+  res.status(200).json(res.locals.result);
+});
+
+app.get("/search", getResults, search, (req, res) => {
+  res.status(200).json(res.locals.result);
+});
+
+app.post("/search", postResults, search, (req, res) => {
+  res.status(200).json(res.locals.result);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}\n-------------------------`);
+});
